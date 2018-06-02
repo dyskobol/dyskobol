2430504d195452c20a34d23ebb23213d336d9403
Jakub Ptak <solpatium003@gmail.com>
2018-06-02 14:25:23 +0200

    Added program termination

--- core/src/main/scala/pl/dyskobol/prototype/DyskobolSystem.scala
+++ core/src/main/scala/pl/dyskobol/prototype/DyskobolSystem.scala
@@ -3,6 +3,7 @@ package pl.dyskobol.prototype
 import java.util.concurrent.CountDownLatch
 
 import akka.actor.{ActorSystem, Props}
+import akka.dispatch.ExecutionContexts
 import akka.stream.scaladsl.Sink
 import pl.dyskobol.prototype.plugin._
 import org.apache.commons.imaging.Imaging
@@ -14,9 +15,10 @@ import akka.stream.{ActorMaterializer, Materializer}
 object Main extends App {
   implicit val system = ActorSystem("dyskobol")
   implicit val materializer = ActorMaterializer()
+  implicit val executionContext = ExecutionContexts.global();
 
   stages.FileSource("./core/res/test.iso").map(f => {
     println( f"${f.path}/${f.name} : ${f.mime}")
     f
-  }).runWith(Sink.ignore)
+  }).runWith(Sink.ignore).onComplete { _ =>  system.terminate() }
 }
