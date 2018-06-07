406ef241b2d6d718546ccc5c2268a0c5c0fa9fa0
Przemysław Moskała <przemyslaw.moskala@gmail.com>
2018-06-07 19:41:21 +0200

    Added forEach strategy

--- core/src/main/scala/pl/dyskobol/prototype/stages/ForEach.scala
+++ core/src/main/scala/pl/dyskobol/prototype/stages/ForEach.scala
@@ -1,8 +1,11 @@
 package pl.dyskobol.prototype.stages
 
+import akka.stream.ActorAttributes.SupervisionStrategy
+import akka.stream.Supervision.{Restart, Resume, Stop}
 import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
-import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
+import akka.stream._
 import pl.dyskobol.model.FlowElements
+
 
 class ForEach[A](f: (FlowElements) => Unit) extends GraphStage[FlowShape[FlowElements,FlowElements]] {
 
@@ -11,12 +14,24 @@ class ForEach[A](f: (FlowElements) => Unit) extends GraphStage[FlowShape[FlowEle
 
   override val shape: FlowShape[(FlowElements), (FlowElements)] = FlowShape.of(in, out)
 
+
   override def createLogic(attr: Attributes): GraphStageLogic =
     new GraphStageLogic(shape) {
+      def decider = attr.get[SupervisionStrategy].map(_.decider).getOrElse(Supervision.resumingDecider)
       setHandler(in, new InHandler {
         override def onPush(): Unit = {
           val (file , fileProp) = grab(in)
-          f(file, fileProp)
+
+          try{
+            f(file, fileProp)
+          } catch {
+            case e: Throwable =>
+              decider(e) match {
+                case Stop => failStage(e)
+                case _ => pull(in)
+            }
+
+          }
           push(out, (file , fileProp))
         }
       })
