733cfc4b7a65a621a6fa2d22c7730e875926e061
Kuba Ptak <jakub.ptak@swmansion.com>
2018-11-14 18:59:15 +0100

    Added bindings test

--- core/src/test/scala/bindings/SleuthkitTest.scala
+++ core/src/test/scala/bindings/SleuthkitTest.scala
@@ -1,10 +1,11 @@
 package bindings
 
 import java.io.FileNotFoundException
+import java.nio.file.{FileSystems, Files, Path}
 
-import org.scalatest.FunSuite
+import org.scalatest._
 
-class SleuthkitTest extends FunSuite{
+class SleuthkitTest extends FunSuite with Matchers {
   test("check if openImgNat throws a proper exception") {
     assertThrows[FileNotFoundException] {
       Sleuthkit.openImgNat("path/that/doesn't/exist")
@@ -15,5 +16,24 @@ class SleuthkitTest extends FunSuite{
     assertThrows[FileNotFoundException] {
       Sleuthkit.openFsNat(0)
     }
+  }
+
+  test("readNat") {
+    val filesystem = Sleuthkit.openFsNat( Sleuthkit.openImgNat("./core/res/test.iso") )
+    val file = Sleuthkit.openFileNat(filesystem, Sleuthkit.getFileInode(filesystem, "./IMAGES/JPG/INVALID.JPG"))
+    val expected = Files.readAllBytes(FileSystems.getDefault.getPath("./core/res/invalid.jpg"))
+    val read = Sleuthkit.readNat(file, 0, expected.size)
+
+    read should be (expected)
+  }
+
+  test("readNatToBuf") {
+    val filesystem = Sleuthkit.openFsNat( Sleuthkit.openImgNat("./core/res/test.iso") )
+    val file = Sleuthkit.openFileNat(filesystem, Sleuthkit.getFileInode(filesystem, "./IMAGES/JPG/INVALID.JPG"))
+    val expected = Files.readAllBytes(FileSystems.getDefault.getPath("./core/res/invalid.jpg"))
+    val read = Array.ofDim[Byte](expected.size)
+    Sleuthkit.readToBufferNat(file, 0, expected.size, read, 0)
+
+    read should be (expected)
   }
 }
